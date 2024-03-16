a42b5c9749c13601d6a2f5231979cc287815ac91
maleehafarrukh <159472134+maleehafarrukh@users.noreply.github.com>
2024-03-16 20:51:45 +0500

    Add files via upload

---
+++ array.ts
@@ -0,0 +1,13 @@
+// array 
+let fruits = ["Apple", "Banana", "Pineapple", "orange"]
+console.log(fruits[0]);
+console.log(fruits.length);
+
+// Adding element to the array
+ fruits.push("grape");
+ console.log(fruits); // output: ["Apple", "Banana", "Pineapple", "orange", "grape"]
+
+ // Removing element to the array
+ let removedFruit = fruits.pop();
+ console.log(removedFruit); //output: "grape"
+ console.log(fruits); // output: ["Apple", "Banana", "Pineapple", "orange"]
