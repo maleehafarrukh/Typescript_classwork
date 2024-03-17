6be4d85cca1c86fb6b7843a8ae60889975fe3ee5
maleehafarrukh <159472134+maleehafarrukh@users.noreply.github.com>
2024-03-17 19:14:11 +0500

    Add files via upload

---
+++ object.js
@@ -0,0 +1,15 @@
+// object
+var person = {
+    firstName: "Ahmed",
+    lastName: "Ali",
+    age: "40",
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        country: "USA"
+    },
+    hobbies: ["painting", "reading", "gardening"]
+};
+console.log(person.firstName); // output: Ahmed
+console.log(person.address.city); // output: Anytown
+console.log(person.hobbies[0]); // output: painting
---
+++ object.ts
@@ -0,0 +1,15 @@
+// object
+let person = {
+    firstName: "Ahmed",
+    lastName: "Ali",
+    age: "40",
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        country: "USA"
+    },
+    hobbies:["painting","reading", "gardening"]
+};
+console.log(person.firstName); // output: Ahmed
+console.log(person.address.city); // output: Anytown
+console.log(person.hobbies[0]); // outpit: painting
